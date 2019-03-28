2cdf4b1062a778c8ef7c718c34d8fb0c61d8c274
Ilya Kopeshtianski <lupusanay@gmail.com>
2019-03-28 20:20:17 +0300

    Fixed static files serving

--- youtube_search/settings.py
+++ youtube_search/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,7 +125,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_ROOT = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Celery
@@ -139,3 +140,5 @@ CELERY_BEAT_SCHEDULE = {
         'schedule': timedelta(minutes=10)
     },
 }
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
